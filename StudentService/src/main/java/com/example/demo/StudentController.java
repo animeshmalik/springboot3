41303ef36e2f3435a351a530c6b86ef5e86f4d23
Animesh Malik <animesh@AnimeshMBP.local>
2024-05-27 00:04:41 +0530

    updating encryption

--- StudentService/src/main/java/com/example/demo/StudentController.java
+++ StudentService/src/main/java/com/example/demo/StudentController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+	@Value("${student.welcomemsg}")
+	private String welcomeMsg;
+
+	@Value("${student.welcomeencryptedmsg}")
+	private String welcomeEncMsg;
 	@GetMapping(value = "/student/details")
 	public String getDetails() {
-		return "studentDetails";
+		return welcomeMsg+ " student and your passphrase is "+welcomeEncMsg;
 	}
 }
---
+++ configserver/src/main/java/com/example/configserver/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.configserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                                .requestMatchers("/decrypt").authenticated()
+                                .requestMatchers("/encrypt").permitAll()
+                                .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
