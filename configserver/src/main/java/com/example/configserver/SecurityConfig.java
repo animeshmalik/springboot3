a7d40bdc563bc031329047caee271cf220e9a277
Animesh Malik <animesh@AnimeshMBP.local>
2024-05-27 00:41:12 +0530

    correction

--- configserver/src/main/java/com/example/configserver/SecurityConfig.java
+++ configserver/src/main/java/com/example/configserver/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-                                .requestMatchers("/decrypt").authenticated()
+                                .requestMatchers("/decrypt").hasAnyRole("DDOADMIN")
                                 .requestMatchers("/encrypt").permitAll()
                                 .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
